7b1ac98f3b05969f7f6c0437c0685eae6c078cc1
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2022-01-27 13:09:06 +1300

    add root end point api

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
 import express, { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient
+const prisma = new PrismaClient()
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello')
+app.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'helo' })
 })
 
 app.listen(port, () => {

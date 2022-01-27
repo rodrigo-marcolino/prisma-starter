921e8b19432b64fab3e733f231c977b9635bedb3
Rodrigo Marcolino <rodrigomarcolino@gmail.com>
2022-01-27 13:45:43 +1300

    add API /products

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,17 @@ import express, { Request, Response } from 'express'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
+
 const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'helo' })
+})
+
+app.get('/products', async (req: Request, res: Response) => {
+  const products = await prisma.product.findMany()
+  res.json(products)
 })
 
 app.listen(port, () => {
